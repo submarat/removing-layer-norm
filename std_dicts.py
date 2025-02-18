9605d5c37e66f48c74b55571c8dade8ec1184337
Marat Subkhankulov <submarat@amazon.com>
2025-02-17 22:23:17 -0800

    Create std values and configs for testing gpt2 medium, large, xl

--- std_dicts.py
+++ std_dicts.py
@@ -2,7 +2,7 @@ import json
 
 # Load standard deviations for each model
 std_dicts = {}
-for model_name in ['gpt2', 'gpt2-medium']:  # Add other model names as needed
+for model_name in ['gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl']:  # Add other model names as needed
     try:
         with open(f'{model_name}_std_dicts.json', 'r') as f:
             std_dicts[model_name] = json.load(f)
