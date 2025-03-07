efab31e32a9cc21457be20d28a2324030df92737
Marat Subkhankulov <submarat@amazon.com>
2025-03-06 20:13:57 -0800

    Log removal event to wandb

--- train.py
+++ train.py
@@ -306,40 +306,44 @@ def finetune_without_ln(model, training_args, tokenized, data_collator, config, 
         Schedules the "removal" of LayerNorms by calling the disable function.
         """
 
-        def __init__(self, start_step, layer_gap_steps, function):
+        def __init__(self, start_step, layer_gap_steps, function, name):
             self.n_layers = len(model.transformer.h)  # Get layers dynamically
             self.start_step = start_step
             self.layer_gap_steps = layer_gap_steps
             self.function = function
+            self.name = name
 
         def __call__(self, step):
             if self.layer_gap_steps is None:
-                # LN functions without layer (i.e. ln_f)
                 if step == self.start_step:
-                    print(f"step {step}")
                     self.function()
+                    self.log_event(step)
             elif self.layer_gap_steps == 0:
-                # LNs where we disable all layers at once
                 if step == self.start_step:
-                    print(f"step {step}")
                     [self.function(i) for i in range(self.n_layers)]
+                    self.log_event(step)
             elif (step - self.start_step) % self.layer_gap_steps == 0:
-                # LNs where we disable one layer at a time
                 layer_index = (step - self.start_step) // self.layer_gap_steps
                 if 0 <= layer_index < self.n_layers:
-                    print(f"step {step}")
                     self.function(layer_index)
+                    self.log_event(step, layer_index)
             else:
-                # Not at a step where we need to disable a LN
                 pass
 
+        def log_event(self, step, layer_index=None):
+            if _USE_WANDB:
+                event_name = f"{self.name}_disabled"
+                if layer_index is not None:
+                    event_name += f"_layer_{layer_index}"
+                # Log a nan value - it will show up at the top of the graph in wandb
+                wandb.log({event_name: float('nan')})
+
         def log(self, wandb):
-            name = self.function.__name__
             if _USE_WANDB:
                 wandb.log(
                     {
-                        f"{name}.start_step": self.start_step,
-                        f"{name}.layer_gap_steps": self.layer_gap_steps,
+                        f"{self.name}.start_step": self.start_step,
+                        f"{self.name}.layer_gap_steps": self.layer_gap_steps,
                     }
                 )
 
@@ -359,12 +363,12 @@ def finetune_without_ln(model, training_args, tokenized, data_collator, config, 
     training_config = config
     
     ln_removers = [
-        LNRemover(training_config.start_ln2, training_config.gap_ln2, disable_ln_2),
-        LNRemover(training_config.start_ln1qk, training_config.gap_ln1qk, disable_ln_1qk),
-        LNRemover(training_config.start_ln1v, training_config.gap_ln1v, disable_ln_1v),
-        LNRemover(training_config.start_lnf, training_config.gap_lnf, disable_ln_f),
-        LNRemover(training_config.start_eot, training_config.gap_eot, disable_eot_std),
-        LNRemover(training_config.start_bos, training_config.gap_bos, disable_bos_std),
+        LNRemover(training_config.start_ln2, training_config.gap_ln2, disable_ln_2, "ln_2"),
+        LNRemover(training_config.start_ln1qk, training_config.gap_ln1qk, disable_ln_1qk, "ln_1qk"),
+        LNRemover(training_config.start_ln1v, training_config.gap_ln1v, disable_ln_1v, "ln_1v"),
+        LNRemover(training_config.start_lnf, training_config.gap_lnf, disable_ln_f, "ln_f"),
+        LNRemover(training_config.start_eot, training_config.gap_eot, disable_eot_std, "eot_std"),
+        LNRemover(training_config.start_bos, training_config.gap_bos, disable_bos_std, "bos_std"),
     ]
 
     # If resuming, apply all removals that should have happened up to resume_step
