f83570a7b1781cf7cde2a4ca3c71abb6c5a56efc
Joachim Schaeffer <joachim.schaeffer@posteo.de>
2025-05-07 22:54:16 +0000

    bs iters

--- config.py
+++ config.py
@@ -436,8 +436,8 @@ def make_gpt2_large_aux():
     n_layers = 36
     
     # Training params
-    base_batch_size = 15
-    max_steps = 1200
+    base_batch_size = 12
+    max_steps = 900
     block_size = 1024
     target_batch_tokens = 2**19
     
