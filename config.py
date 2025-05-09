dd5a5d118487bbc227d3b9bb7262f93e354b01f7
root <joachim.schaeffer@posteo.de>
2025-05-09 07:14:41 +0000

    config

--- config.py
+++ config.py
@@ -436,7 +436,7 @@ def make_gpt2_large_aux():
     n_layers = 36
     
     # Training params
-    base_batch_size = 12
+    base_batch_size = 15
     max_steps = 1200
     block_size = 1024
     target_batch_tokens = 2**19
